6ef5ca461cd3ccfe48e91f6b468496367aed4f74
samanfathnazarian <saman.nazarian17@gmail.com>
2023-09-27 23:02:12 -0500

    Implemented Linking Between Pages

--- nextjs-practice/pages/news/[newsid].js
+++ nextjs-practice/pages/news/[newsid].js
@@ -1,6 +1,17 @@
+import { useRouter } from 'next/router';
+
 // our-domain.com/news/something-important
 
 function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  // send a request to the backend API
+  // to fetch the news item with newsID
+
+  // console.log(newsId);
+
   return <h1>The Detail Page</h1>;
 }
 
--- nextjs-practice/pages/news/index.js
+++ nextjs-practice/pages/news/index.js
@@ -1,6 +1,19 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 // domain.com/news
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <ul>
+        <li>
+          {/* preserves states across pages, makes a single page application*/}
+          <Link href="/news/nextjs-is-a-great-framework">NextJS Is A Great Framework</Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
